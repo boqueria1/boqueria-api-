ca23978e1f11cc860d4d3cbfee84ac25e3a7426f
boqueria1 <boqueria1@gmail.com>
2025-07-01 16:59:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # --- 設定変数（Renderに登録済の値をそのまま使用） ---
-GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzx2mUTX2jDs68z0u6YRAiez41-wmoVKsQAo9lhHMC1eZ36JuRjEPe02nX3Q0CuDDUtQw/exec"
+GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxM64weIf2Hy-KGUpcSRUZ5vJziy3icdL6jtjfokQNzBOe7HcP5bBx_T213QV6wucD8/exec"
 INTERNAL_API_KEY = "QUIZ_APP_INTERNAL_API_SECRET_2025"
 INTERNAL_API_KEY_HEADER_NAME = "X-Internal-API-Key"
 
